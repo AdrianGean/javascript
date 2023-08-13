d4a9be5da9f7e0130d548eab6384fe3e961e275d
Adrian Gean <124299369+AdrianGean@users.noreply.github.com>
2023-08-13 18:43:39 -0400

    js

--- js/fundamentos.js
+++ js/fundamentos.js
@@ -122,5 +122,137 @@ switch(fruta) {
         console.log('Fruta não encontrada')
 }
 
+// Estruturas de repetição
+// 1, 2, 3, 4, 5, 6... => dependendo de uma condição
 
 
+//contador, condição de limite, incremento
+for (let i = 0; i < 5; i++) {
+    console.log(`O valor de i é ${i}`)
+}
+
+//while
+let k = 0
+
+while (k < 5) {
+    console.log(`O valor de k é ${k}`)
+    k++
+}
+
+// do while
+
+let j = 0
+
+do {
+    console.log(`O valor de j é ${j}`)
+    j++
+} while (j < 5)
+
+
+//funções
+// function nomedafunção (arg1, arg2) {corpo}
+// arg são como variáveis passadas pra função que podem ser usadas dentro dela
+function cumprimentar (nome) {
+    console.log('Olá ' + nome)
+}
+
+// invocação = nome()
+cumprimentar('Matheus')
+
+
+// escopo de variáveis
+let cor = 'azul'
+
+
+function mostrarCor() {
+    let cor = 'verde'
+    console.log(cor)
+}
+
+console.log(cor)
+
+mostrarCor()
+
+//hoisting = içamento
+
+testeHoisting()
+
+
+function testeHoisting() {
+    console.log('Deu certo')
+}
+
+
+// arrow function
+const testeArrow = () => console.log('Isso também é uma função')
+
+
+testeArrow()
+
+//truthy e falsy
+const minhaVariavel1 = '' //falsy, o JS considera falso pq não tem nenhum valor
+const minhaVariavel2 = 'algum texto' //truthy, o JS considera verdadeiro pq tem valor
+
+if (minhaVariavel1) {
+    console.log("É verdadeiro")
+} else {
+    console.log("É falso")
+}
+
+if (minhaVariavel2) {
+    console.log("É verdadeiro 2")
+} else {
+    console.log("É falso 2")
+}
+
+// array, listas 
+const numeros = [1, 2, 3, 4, 5]
+
+console.log(numeros)
+
+console.log(numeros[2])
+
+numeros.push(6)
+
+console.log(numeros)
+
+//prototype => OBJETO => objeto2, pega um objeto e permite derivar todas as caracteristicas dele para outro objeto.
+// Array => nossos arrays, posso usar o método pop,push,log, etc pq o Array pode usar, e os nossos arrays são derivados do Array, logo podemos usar também.
+
+numeros.pop()
+
+console.log(numeros)
+
+
+//strings
+const minhaStringNova = 'Olá, Mundo!'
+
+//concatenação = +
+const minhaString3 = minhaStringNova + ' Como você está?'
+
+console.log(minhaString3)
+
+
+//interpolação
+const minhaString4 = `${minhaStringNova} Como você está?`
+console.log(minhaString4)
+
+console.log(minhaString4.length) // qtd de caracteres
+console.log(minhaString4[5])
+console.log(minhaString4.toUpperCase())
+
+// Data e hora
+const agora = Date () //pegamos a data atual com esse objeto
+console.log(agora)
+
+const natal =  new Date(2023, 11, 25)
+console.log(natal)
+
+// Math
+console.log(Math.PI) // da o valor do PI
+
+console.log(Math.round(3.6)) //arredondar um número
+
+console.log(Math.sqrt(16)) // raiz quadrada
+
+console.log(Math.pow(2,3)) // potência, base 2 e expoente 3 
