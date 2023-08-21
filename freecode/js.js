637eb5d1201cbb8eb621bc4f2c4ce768cacaa3c7
Adrian Gean <124299369+AdrianGean@users.noreply.github.com>
2023-08-21 19:49:24 -0400

    js

--- freecode/js.js
+++ freecode/js.js
@@ -13,3 +13,38 @@ function nextInLine(arr, item) { // (arr = testArr = [1, 2, 3, 4, 5],           
   console.log("Before: " + JSON.stringify(testArr));
   console.log(nextInLine(testArr, 6)); // o pulo do gato é entender que a função nextInLine tá sendo chamada com os argumentos (testArr, 6)
   console.log("After: " + JSON.stringify(testArr));
+
+
+
+
+let count = 0;
+
+function cc(card) {
+  // Altere apenas o código abaixo desta linha
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6: 
+      count++ 
+      break
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count-- 
+      break
+  }
+  let holdbet = 'Hold'
+  if (count > 0) {
+    holdbet = 'Bet'
+  }
+ 
+
+  return count + ' ' + holdbet;
+  // Altere apenas o código acima desta linha
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
